859cc56cddb82b9048ea492716d72b595ad18e08
Vladislav Lukashevich <vladislav.lukashevich@emlid.com>
2023-09-18 13:13:27 +0300

    Quotes model: tied the fetchQuote to the handleGetQuote via forward

--- src/models/quotes/init.ts
+++ src/models/quotes/init.ts
@@ -1,12 +1,22 @@
-import {createEffect, createStore} from "effector";
+import {createEffect, createEvent, createStore, forward} from "effector";
 import axios from "axios";
 
-export const handleGetQuote = createEffect(async (url: string): Promise<string> => {
+const handleGetQuote = createEffect(async (url: string): Promise<string> => {
     const res =  await axios.get(url);
 
     return res.data.quote;
 })
 
+export const fetchQuote = createEvent<string>();
+
+forward({
+    from: fetchQuote,
+    to: handleGetQuote
+})
+
 export const $quotesStore = createStore<string[]>([])
-    .on(handleGetQuote.doneData, (state, newQuote) => [newQuote, ...state]);
+    .on(handleGetQuote.doneData, (state, newQuote) => {
+        console.log(newQuote)
+        return [newQuote, ...state]
+    });
 
--- src/pages/quotes/quotes.tsx
+++ src/pages/quotes/quotes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Button, List} from "antd";
-import {handleGetQuote, $quotesStore} from "../../models/quotes/init";
+import {fetchQuote, $quotesStore} from "../../models/quotes/init";
 import {useStore} from "effector-react";
 
 const quotesUrl: string = "https://api.kanye.rest";
@@ -11,7 +11,7 @@ export const Quotes: React.FC = () => {
     return (
         <List
             size="small"
-            header={<Button onClick={() => handleGetQuote(quotesUrl)}>Get new quote</Button>}
+            header={<Button onClick={() => fetchQuote(quotesUrl)}>Get new quote</Button>}
             dataSource={quotesStore}
             renderItem={(item) => <List.Item>{item}</List.Item>}
         />
